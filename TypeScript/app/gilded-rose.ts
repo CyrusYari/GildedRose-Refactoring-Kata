493a8fd8b9ad7c04550951d17070a855a21598cf
cyrusyari <cyrus.brk@gmail.com>
2023-03-12 21:39:51 +0000

    progress on typescript, need to add conjured items and test it next

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -3,12 +3,67 @@ export class Item {
   sellIn: number;
   quality: number;
 
-  constructor(name, sellIn, quality) {
+  constructor(name: string, sellIn: number, quality: number) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
 }
+
+
+const ItemTypes = {
+  BRIE: 'Aged Brie',
+  PASS: 'Backstage passes to a TAFKAL80ETC concert',
+  HAND: 'Sulfuras, Hand of Ragnaros',
+  CONJ: 'Conjured',
+}
+
+
+function updateBrie(item: Item) {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+  }
+  item.sellIn = item.sellIn - 1;
+  if (item.sellIn < 0 && item.quality < 50) {
+    item.quality = item.quality + 1;
+  }
+}
+
+function updatePass(item: Item) {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+      if (item.sellIn < 11 && item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+      if (item.sellIn < 6 && item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    }
+    item.sellIn = item.sellIn - 1;
+  if (item.sellIn < 0) {
+    item.quality = item.quality - item.quality;
+  }
+}
+
+function updateHand(item: Item) {
+}
+
+
+function updateNormal(item: Item) {
+  if (item.quality > 0) {
+    item.quality = item.quality - 1;
+  }
+  item.sellIn = item.sellIn - 1;
+  if (item.sellIn < 0 && item.quality > 0) {
+    item.quality = item.quality - 1;
+  }
+}
+
+
+function updateConj(item: Item) {
+}
+
+
 
 export class GildedRose {
   items: Array<Item>;
@@ -18,52 +73,78 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
-    }
+    for (const item of this.items) {
 
-    return this.items;
+      switch(item.name) {
+        case ItemTypes.BRIE:
+          updateBrie(item);
+          continue;
+        case ItemTypes.HAND:
+          updateHand(item);
+          continue;
+        case ItemTypes.PASS:
+          updatePass(item);
+          continue;
+      case ItemTypes.CONJ:
+          updateConj(item);
+          continue;  
+        default:
+          updateNormal(item);
+          continue;
+      }
+
   }
+return this.items;
+
 }
+}
+
+// module.exports = {
+//   Item,
+//   GildedRose
+// }
+
+
+// import { Item } from './item';
+// import { 
+//   updateQualityForAgedBrie, 
+//   updateQualityForConcert, 
+//   updateQualityForSulfuras, 
+//   updateQualityForConjured, 
+//   updateQualityForNormalItem} from './updatedQuality'
+
+// export class GildedRose {
+//   items: Array<Item>;
+
+//   constructor(items = [] as Array<Item>) {
+//       this.items = items;
+//   }
+
+//   updateQuality() :Item[]  {
+//       this.items.forEach(currentItem => {
+          
+//           switch (currentItem.name) {
+//               case 'Aged Brie': {
+//                   currentItem = updateQualityForAgedBrie(currentItem)
+//                   break;
+//               }
+//               case 'Backstage passes to a TAFKAL80ETC concert': {
+//                       currentItem = updateQualityForConcert(currentItem)
+//                       break;
+//               }
+//               case  'Sulfuras, Hand of Ragnaros':  {
+//                       currentItem = updateQualityForSulfuras(currentItem)
+//                       break;
+//               }
+//               case 'Conjured':  {
+//                       currentItem = updateQualityForConjured(currentItem)
+//                       break;
+//               }
+//               default: {
+//                       currentItem = updateQualityForNormalItem(currentItem)
+//               }
+//           }
+//       })
+//       return this.items;
+//   }
+// }
